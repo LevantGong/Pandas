a9b4f213ccfd6481101f3c3261e951c103163511
YushangGong <Yushang.gong@otis.com>
2023-03-09 16:11:26 +0800

    OTAGroup分离R和S版本

--- python/OTA.py
+++ python/OTA.py
@@ -1,10 +1,22 @@
 import pandas as pd
 import numpy as np
 pd.set_option('display.unicode.east_asian_width',True)
-df = pd.read_excel("./xlsx/exer.xlsx")
-data = df['GatewayID']
-dd = pd.DataFrame(data)
+df1 = pd.read_excel("./xlsx/exer1.xlsx")
+dfR = df1[df1['currentAppVersion'].str.contains("R",na=False)]
+dfS = df1[df1['currentAppVersion'].str.contains("S",na=False)]
 
-testdf = pd.DataFrame(dd.values.T,columns=dd.index,index=dd.columns)
-testdf.to_csv("./csv/test6.csv",index=False,header=False)
-print(testdf)
+dataR = dfR['GatewayID']
+dataS = dfS['GatewayID']
+
+DR = pd.DataFrame(dataR)
+DS = pd.DataFrame(dataS)
+DFR = pd.DataFrame(DR.values.T,columns=DR.index,index=DR.columns)
+DFS = pd.DataFrame(DS.values.T,columns=DS.index,index=DS.columns)
+print(DFR)
+print(DFS)
+if DFR.empty == False:
+    DFR.to_csv("./OTAGroup/Gateway_R.csv",index=False,header=False)
+if DFS.empty == False:
+    DFS.to_csv("./OTAGroup/Gateway_S.csv",index=False,header=False)
+
+
